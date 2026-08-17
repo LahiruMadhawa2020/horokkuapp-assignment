3ec7f25496b450dcacc8754b236071bd2047719a
Lahiru Madhawa <kmlahiru24@gmail.com>
2026-08-17 19:22:43 +0530

    added the project with updated config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,11 +10,14 @@ export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 1 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
     trace: 'retain-on-failure',
+    video: 'retain-on-failure',
+    screenshot: 'only-on-failure',
+    // baseURL: process.env.BASE_URL || 'https://the-internet.herokuapp.com/',
   },
 
   projects: [
